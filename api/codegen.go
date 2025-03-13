35577345bc3e79d39219e78d20f89dea4e9c0739
Tomas Aschan <tomasl@spotify.com>
2025-03-13 01:32:28 +0100

    Configure controller-gen for object/deepcopy

---
+++ api/codegen.go
@@ -0,0 +1,8 @@
+// These generate directives are kept separate from the main package to enable
+// running them without also running the license headers and attribution checks.
+//
+// To run everything, from the repo root do go generate ./...
+// To run just the CRD related codegen, from the repo root do go generate ./api/...
+package api
+
+//go:generate go tool controller-gen object paths="./..." output:object:artifacts:config=../helm/templates/crds

99aaa16d487dc2e1de74cf1df971ca5c340a9d3c
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-03-18 01:45:35 +0530

    pkg graph/metadata/selectors.go: adds unit tests for selectors

---
+++ pkg/metadata/selectors_test.go
@@ -0,0 +1,68 @@
+package metadata
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// testObject is a mock implementation specific to selector tests
+type testObject struct {
+	metav1.ObjectMeta
+}
+
+func newTestObject(name, namespace string, uid types.UID) *testObject {
+	return &testObject{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			UID:       uid,
+		},
+	}
+}
+
+func TestNewInstanceSelector(t *testing.T) {
+	instance := newTestObject("test-instance", "test-ns", "instance-123")
+	selector := NewInstanceSelector(instance)
+
+	assert.Equal(t, map[string]string{
+		InstanceIDLabel: "instance-123",
+	}, selector.MatchLabels)
+}
+
+func TestNewResourceGraphDefinitionSelector(t *testing.T) {
+	rgd := newTestObject("test-rgd", "test-ns", "rgd-123")
+	selector := NewResourceGraphDefinitionSelector(rgd)
+
+	assert.Equal(t, map[string]string{
+		ResourceGraphDefinitionIDLabel: "rgd-123",
+	}, selector.MatchLabels)
+}
+
+func TestNewInstanceAndResourceGraphDefinitionSelector(t *testing.T) {
+	instance := newTestObject("test-instance", "test-ns", "instance-123")
+	rgd := newTestObject("test-rgd", "test-ns", "rgd-123")
+
+	selector := NewInstanceAndResourceGraphDefinitionSelector(instance, rgd)
+
+	assert.Equal(t, map[string]string{
+		InstanceIDLabel:                "instance-123",
+		ResourceGraphDefinitionIDLabel: "rgd-123",
+	}, selector.MatchLabels)
+}
+
+func TestNewNodeAndInstanceAndResourceGraphDefinitionSelector(t *testing.T) {
+	node := newTestObject("test-node", "test-ns", "node-123")
+	instance := newTestObject("test-instance", "test-ns", "instance-123")
+	rgd := newTestObject("test-rgd", "test-ns", "rgd-123")
+
+	selector := NewNodeAndInstanceAndResourceGraphDefinitionSelector(node, instance, rgd)
+
+	assert.Equal(t, map[string]string{
+		NodeIDLabel:                    "test-node",
+		InstanceIDLabel:                "instance-123",
+		ResourceGraphDefinitionIDLabel: "rgd-123",
+	}, selector.MatchLabels)
+}

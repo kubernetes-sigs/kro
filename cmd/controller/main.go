5e38f9136d59d12191b7ade1bf567757f2633220
Samson S. Kolge <eglok1980@gmail.com>
2025-03-17 06:57:59 +0530

    feat: Add GitOps application support with Flux CD integration
    Signed-off-by: Samson S. Kolge <eglok1980@gmail.com>

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -78,7 +78,7 @@ func main() {
 	)
 
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8078", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8079", "The address the probe endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8080", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")

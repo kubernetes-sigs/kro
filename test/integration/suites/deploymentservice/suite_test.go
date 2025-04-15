c613552e8d892c5cdf0af6767041fdf5d5b21249
Tomas Aschan <tomasl@spotify.com>
2025-04-15 21:32:21 +0200

    Add back incorrectly removed package directives

--- test/integration/suites/deploymentservice/suite_test.go
+++ test/integration/suites/deploymentservice/suite_test.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+package deploymentservice_test
+
 import (
 	"context"
 	"fmt"
--- test/integration/suites/networkingstack/suite_test.go
+++ test/integration/suites/networkingstack/suite_test.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+package networkingstack_test
+
 import (
 	"context"
 	"fmt"

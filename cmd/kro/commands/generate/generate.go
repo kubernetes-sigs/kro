942d1fb36237e1502444bdfede3409b063919bdc
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2025-08-14 21:59:53 +0530

    segregate utility functions

--- cmd/kro/commands/generate/generate.go
+++ cmd/kro/commands/generate/generate.go
@@ -15,14 +15,7 @@
 package generate
 
 import (
-	"encoding/json"
-	"fmt"
-
-	"github.com/kro-run/kro/api/v1alpha1"
-	kroclient "github.com/kro-run/kro/pkg/client"
-	"github.com/kro-run/kro/pkg/graph"
 	"github.com/spf13/cobra"
-	"sigs.k8s.io/yaml"
 )
 
 type GenerateConfig struct {
@@ -43,53 +36,6 @@ var generateCmd = &cobra.Command{
 	Short: "Generate the CRD from a given ResourceGraphDefinition",
 	Long: "Generate the CRD from a given ResourceGraphDefinition." +
 		"This command generates a CustomResourceDefinition (CRD) based on the provided ResourceGraphDefinition file.",
-}
-
-func createGraphBuilder(rgd *v1alpha1.ResourceGraphDefinition) (*graph.Graph, error) {
-	set, err := kroclient.NewSet(kroclient.Config{})
-	if err != nil {
-		return nil, fmt.Errorf("failed to create client set: %w", err)
-	}
-
-	restConfig := set.RESTConfig()
-
-	builder, err := graph.NewBuilder(restConfig)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create graph builder: %w", err)
-	}
-
-	rgdGraph, err := builder.NewResourceGraphDefinition(rgd)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create resource graph definition: %w", err)
-	}
-
-	return rgdGraph, nil
-}
-
-func marshalObject(obj interface{}, outputFormat string) ([]byte, error) {
-	var b []byte
-	var err error
-
-	if obj == nil {
-		return nil, fmt.Errorf("nil object provided for marshaling")
-	}
-
-	switch outputFormat {
-	case "json":
-		b, err = json.Marshal(obj)
-		if err != nil {
-			return nil, err
-		}
-	case "yaml":
-		b, err = yaml.Marshal(obj)
-		if err != nil {
-			return nil, err
-		}
-	default:
-		return nil, fmt.Errorf("unsupported output format: %s", outputFormat)
-	}
-
-	return b, nil
 }
 
 func AddGenerateCommands(rootCmd *cobra.Command) {

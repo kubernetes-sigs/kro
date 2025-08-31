59070f7e724ede37c71f4ec3b2a155b285fcec6a
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2025-08-31 14:11:49 +0530

    kro cli publish command

--- cmd/kro/commands/root.go
+++ cmd/kro/commands/root.go
@@ -18,10 +18,12 @@ import (
 	"github.com/spf13/cobra"
 
 	generate "github.com/kro-run/kro/cmd/kro/commands/generate"
+	publish "github.com/kro-run/kro/cmd/kro/commands/publish"
 	validate "github.com/kro-run/kro/cmd/kro/commands/validate"
 )
 
 func AddCommands(root *cobra.Command) {
 	generate.AddGenerateCommands(root)
 	validate.AddValidateCommands(root)
+	publish.AddPublishCommand(root)
 }

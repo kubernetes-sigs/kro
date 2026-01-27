fc7c3d1b85a0aff9061d6c7dd87b9430aa923a59
Jakob Möller <jakob.moeller@sap.com>
2026-01-27 11:26:59 +0100

    docs: cutoff 0.8.0
    Signed-off-by: Jakob Möller <jakob.moeller@sap.com>

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -91,7 +91,7 @@ const config: Config = {
           editUrl: "https://github.com/kubernetes-sigs/kro/tree/main/website",
           disableVersioning: false,
           includeCurrentVersion: true,
-          lastVersion: "0.7.1",
+          lastVersion: "0.8.0",
         },
         blog: false,
         theme: {
